cf3d16ce78c13bd035875ecbebef311063125667
Samuel Chan <emschan@live.cn>
2024-05-10 23:24:39 +0800

    Add `cancel` capability

--- src/main/java/io/github/shangor/gateway/OllamaProxyController.java
+++ src/main/java/io/github/shangor/gateway/OllamaProxyController.java
@@ -11,8 +11,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @RestController
 @CrossOrigin(origins = {"*"})
@@ -23,6 +27,8 @@ public class OllamaProxyController {
     @Resource
     ObjectMapper objectMapper;
 
+    private Set<String> requestPool = new CopyOnWriteArraySet<>();
+
     /**
      * Will only return as text-stream.
      * @param requestText
@@ -31,6 +37,9 @@ public class OllamaProxyController {
      */
     @PostMapping("/chat/ollama")
     public Flux<ServerSentEvent<String>> chat(@RequestBody String requestText, ServerHttpRequest request) {
+        var requestId = UUID.randomUUID().toString();
+        requestPool.add(requestId);
+
         Boolean stream;
         try {
             var m = objectMapper.readValue(requestText, Map.class);
@@ -44,6 +53,8 @@ public class OllamaProxyController {
             return Flux.error(e);
         }
 
+        var cancelDisposable = Schedulers.newSingle(requestId);
+
         WebClient.RequestBodySpec client;
         if (stream) {
             client = WebClient.create(ollamaUrl).post().uri("/api/chat").header("Content-Type", "text/stream-event;charset=utf-8");
@@ -53,10 +64,29 @@ public class OllamaProxyController {
         for (var entry : request.getHeaders().entrySet()) {
             client = client.header(entry.getKey(), entry.getValue().get(0));
         }
-        return client.bodyValue(requestText)
+        var idString = """
+                {"id": "%s", "done": false}""".formatted(requestId);
+        return Flux.just(ServerSentEvent.builder(idString).build())
+                .concatWith(client.bodyValue(requestText)
                 .retrieve()
-                .bodyToFlux(String.class)
-                .map(ollamaChatCompletion -> ServerSentEvent.builder(ollamaChatCompletion).build());
+                .bodyToFlux(String.class).cancelOn(cancelDisposable)
+                .map(ollamaChatCompletion -> {
+                    if (requestPool.contains(requestId)) {
+                        return ServerSentEvent.builder(ollamaChatCompletion).build();
+                    } else {
+                        cancelDisposable.dispose();
+                        return ServerSentEvent.builder("""
+                                {"id": "%s", "done": true}""".formatted(requestId)).build();
+                    }
+                })).doFinally(signal -> {
+                    requestPool.remove(requestId);
+                });
+    }
+
+    @GetMapping(value = "/api/cancel/{requestId}")
+    public String cancel(@PathVariable String requestId) {
+        requestPool.remove(requestId);
+        return "ok";
     }
 
     @GetMapping(value = "/api/tags", produces = "application/json")
