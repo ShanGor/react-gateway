1b1c6206c33ffa3222a717cfefd7acf86cbbe8aa
Samuel Chan <emschan@live.cn>
2025-01-18 11:50:20 +0800

    Add cache control to accelerate the browser loading

--- src/main/java/io/github/shangor/gateway/MediaTypes.java
+++ src/main/java/io/github/shangor/gateway/MediaTypes.java
@@ -1,13 +1,12 @@
 package io.github.shangor.gateway;
 
-import java.util.List;
 import java.util.Map;
 
 /**
- * MIME Types, ref to https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+ * MIME Types, ref to <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types">Common Types</a>
  */
 public class MediaTypes {
-    public static final Map<String, String> MEDIA_TYPES = Map.ofEntries(
+    public static final Map<String, String> MEDIA_TYPES = Map.<String, String>ofEntries(
             Map.entry(".aac", "audio/aac"),
             Map.entry(".abw", "application/x-abiword"),
             Map.entry(".apng", "image/apng"),
@@ -87,5 +86,4 @@ public class MediaTypes {
             Map.entry("7z", "application/x-7z-compressed")
     );
 
-    public static final List<String> SUFFIXES = MEDIA_TYPES.keySet().stream().toList();
 }
